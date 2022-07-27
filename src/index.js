8b3c766f90fec3df3a2bdcea54485e3900bfd0a1
rusland420 <99689446+rusland420@users.noreply.github.com>
2022-07-27 22:56:44 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let nn=Math.abs(n)
+    let x=nn.toString();
+    const arr=[...x];
+    const arrRev=arr.reverse();
+    console.log(arrRev);
+    let str='';
+    for(let i=0;i<arrRev.length;i++){
+        str+=arrRev[i]; 
+    }
+    const result=Number(str);
+    console.log(result);
+    return result;
 }
